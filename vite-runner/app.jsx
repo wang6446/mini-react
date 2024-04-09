74359efce7c04191f180da397f7d68ea0fef4beb
weikai.wang <it.cq@transsion.com>
2024-04-09 13:36:12 +0800

    reset

--- vite-runner/app.jsx
+++
@@ -1,7 +0,0 @@
-import React from "./core/react.js";
-
-// const App = React.createElement("div", { id: "app" }, "mini",'vue');
-
-const App=<div id="app" >mini-vue</div>
-console.log(App,'App');
-export default App;
--- vite-runner/main.js
+++
@@ -1,4 +0,0 @@
-import ReactDom from "./core/reactDom.js";
-import App from "/app.jsx";
-
-ReactDom.creatRoot(document.getElementById("root")).render(App);
